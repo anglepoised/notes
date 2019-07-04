8a4d4a56dc6ec3e5e657e877022ce5c0584ce2e9
Paul Love <paul@anglepoised.com>
2019-07-04 16:53:16 +0100

    feat: allow easy video embeds
    Uses https://www.gatsbyjs.org/packages/gatsby-remark-embed-video/ for simplicity.

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,15 @@ module.exports = {
             },
           },
           {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77,
+              related: false,
+              noIframeBorder: true,
+            },
+          },
+          {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
               wrapperStyle: `margin-bottom: 1.0725rem`,
