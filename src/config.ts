5048c45751db2dcc3b94a0489bee2350bd79a74e
Paul Love <paul@anglepoised.com>
2022-09-04 18:00:04 +0100

    style: prettier all the things

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Anglepoised Notes';
-export const SITE_DESCRIPTION = 'Order coupled with pleasing decoration.';
+export const SITE_TITLE = "Anglepoised Notes";
+export const SITE_DESCRIPTION = "Order coupled with pleasing decoration.";
