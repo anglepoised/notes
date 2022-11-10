8e2ce53511ceae67556518ba6f58427accbe5645
Paul Love <paul@anglepoised.com>
2022-11-10 17:42:54 +0000

    chore: add bookmark post generator

---
+++ _templates/bookmark/new/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: "input",
+    name: "title",
+    message: "Bookmark title (sentence case)?",
+  },
+  {
+    type: "input",
+    name: "bookmarkUrl",
+    message: "Bookmark URL?",
+  },
+];
