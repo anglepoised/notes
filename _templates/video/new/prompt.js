a8d6d8f9957c89f324dbd24848558e56b3c95683
Paul Love <paul@anglepoised.com>
2024-05-13 21:22:09 +0100

    chore: make generator branch creation optional

--- _templates/video/new/prompt.js
+++ _templates/video/new/prompt.js
@@ -4,4 +4,10 @@ module.exports = [
     name: "title",
     message: "Post title (sentence case)?",
   },
+  {
+    type: "confirm",
+    name: "createBranch",
+    message: "Do you want to create a new branch for this article?",
+    default: false, // Default no to avoid accidental branch creation
+  },
 ];
